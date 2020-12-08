530b1aa1d62f2cf10200948a82b4dca77c99316c
Yevhen Pechurin <yevhen.pechurin@n-development.com>
2020-12-08 13:44:39 +0200

    [ADD] outplacement_order_interpretor AFC-1154: added module outplacement_order_interpretor

---
+++ outplacement_order_interpretor/models/res_partner.py
@@ -0,0 +1,19 @@
+# coding: utf-8
+
+from odoo import api, fields, models
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    @api.model
+    def _lang_get(self):
+        return self.env['res.lang'].get_installed()
+
+    interpreter_language = fields.Selection(
+        _lang_get, default=lambda self: self.env.lang)
+    interpreter_gender_preference = fields.Selection([
+        ('male', 'Male'),
+        ('female', 'Female'),
+        ('no_preference', 'no preference'),
+    ])

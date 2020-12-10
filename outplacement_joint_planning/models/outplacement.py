d99b302ddff8ef142362be31498080c1317f9a10
Anatolii Yakymets <anatolii.yakymets@n-development.com>
2020-12-10 17:52:22 +0200

    12.0-AFC-1486a: Fix Added new module

--- outplacement_joint_planning/models/outplacement.py
+++ outplacement_joint_planning/models/outplacement.py
@@ -44,26 +44,4 @@ class Outplacement(models.Model):
         for outplacement in self:
             outplacement.jp_sent_date = date.today()
             joint_plannings = self.env['res.joint_planning'].search([])
-            joint_planning_data = [{"aktivitets_id": joint_planning.activity_id,
-                                    "aktivitets_namn": joint_planning.task_type,
-                                    "beskrivning": joint_planning.name}
-                                   for joint_planning in joint_plannings]
-            payload = {
-                "utforande_verksamhets_id": "10009858",
-                "avrops_id": "A000000398768",
-                "genomforande_referens": "100000123",
-                "ordernummer": "MEET-1",
-                "personnr": "197608277278",
-                "unikt_id": "1321",
-                "deltagare": {
-                    "fornamn": "John",  # outplacement.user_id.partner_id.firstname
-                    "efternamn": "Doe",  # outplacement.user_id.partner_id.lastname
-                    "deltog_per_distans": "yes"
-                },
-                "inskickad_datum": "2020-08-20",
-                "status": "SENT",
-                "innehall": joint_planning_data
-            }
-            from pprint import pprint
-            pprint(payload)
-            client.post_report(payload)
+            client.post_request(outplacement, joint_plannings)

d5d66a1b4bf9c7a0a0dc927e63f3c16e754499f1
Sebastian Gieser <sebastian.gieser@vertel.se>
2020-12-18 15:50:06 +0100

    outplacement_partner_jobs AFC-1567: adds tab to outplacement form view with partner job information

---
+++ outplacement_partner_jobs/models/outplacement.py
@@ -0,0 +1,6 @@
+from odoo import models, fields
+
+class Outplacement(models.Model):
+    _inherit = "outplacement"
+
+    job_ids = fields.One2many(comodel_name="res.partner.jobs", inverse_name="partner_id", related="partner_id.job_ids")

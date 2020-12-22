d8653586e352adb819867184c38dc647bc3730f3
Sebastian Gieser <sebastian.gieser@vertel.se>
2020-12-22 17:20:46 +0100

    outplacement_final_report AFC-1541: initial version, adds tab to outplacement view with obstacles and study visits

---
+++ outplacement_final_report/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Outplacement final report',
+    'version': '12.0.0.1.0',
+    'category': 'Outplacement',
+    'description': """
+	 Module to handle outplacement (Avrop) final reports.
+    """,
+    'author': 'Vertel AB',
+    'license': 'AGPL-3',
+    'website': 'http://www.vertel.se',
+    'depends': ['outplacement'],
+    'data': [
+        'views/outplacement_view.xml'
+    ],
+    'installable': True,
+}
---
+++ outplacement_final_report/models/outplacement.py
@@ -0,0 +1,31 @@
+from odoo import api, fields, models, tools, SUPERUSER_ID
+from odoo import tools, _
+import logging
+_logger = logging.getLogger(__name__)
+
+
+class Outplacement(models.Model):
+    _inherit = 'outplacement'
+
+    study_visit_ids = fields.Many2many(comodel_name="outplacement.study_visit")
+    obstacle_id = fields.Many2one(comodel_name="outplacement.obstacle")
+
+    obstacle_reason = fields.Selection(related="obstacle_id.reason", readonly=False)
+    obstacle_motivation = fields.Text(related="obstacle_id.motivation", readonly=False)
+
+
+class OutplacementObstacle(models.Model):
+    _name = "outplacement.obstacle"
+    
+    outplacement_id = fields.One2many(comodel_name="outplacement", inverse_name="obstacle_id")
+    reason = fields.Selection([('loan', 'Kan inte ta studielån'), ('sickness', 'På grund utav sjukdom'), ('economy', 'På grund utav ekonomisk situation'), ('other', 'Annat')])
+    motivation = fields.Text(string="Motivation")
+
+class OutplacementStudyVisit(models.Model):
+    _name = "outplacement.study_visit"
+
+    outplacement_ids = fields.One2many(comodel_name="outplacement")
+    name = fields.Char(string="Name")
+    visit_type = fields.Char(string="Type")
+    reasoning = fields.Text(string="Reasoning")
+    

2e38240175d5c05b1910f42ff0bc0d2476acb0ce
Sebastian Gieser <sebastian.gieser@vertel.se>
2020-12-23 15:23:04 +0100

    outplacement_final_report_ipf_client AFC-1424: initial copy paste find replace version, dummy data for a ton of fields

---
+++ outplacement_final_report_ipf_client/models/client_config.py
@@ -0,0 +1,317 @@
+# -*- coding: UTF-8 -*-
+
+################################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2019 N-Development (<https://n-development.com>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+################################################################################
+
+from odoo.tools import pycompat
+import json
+import uuid
+import logging
+import requests
+from odoo import api, http, models, tools, SUPERUSER_ID, fields
+
+_logger = logging.getLogger(__name__)
+
+
+class ClientConfig(models.Model):
+    _name = 'ipf.final_report.client.config'
+    _description = 'Final report ipf client'
+    _rec_name = 'url'
+
+    url = fields.Char(string='Url',
+                      required=True)
+    client_secret = fields.Char(string='Client Secret',
+                                required=True)
+    client_id = fields.Char(string='Client ID',
+                            required=True)
+    environment = fields.Selection(selection=[
+        ('U1', 'U1'),
+        ('I1', 'I1'),
+        ('T1', 'IT'),
+        ('T2', 'T2'),
+        ('PROD', 'PROD'),
+    ], string='Environment',
+        default='U1',
+        required=True)
+    request_history_ids = fields.One2many(
+        'ipf.final_report.request.history',
+        'config_id',
+        string='Requests')
+
+    def request_call(self, method, url, payload=None,
+                     headers=None, params=None):
+
+        response = requests.request(method=method,
+                                    url=url,
+                                    data=payload,
+                                    headers=headers,
+                                    params=params,
+                                    verify=False)
+
+        self.create_request_history(method=method,
+                                    url=url,
+                                    response=response,
+                                    payload=payload,
+                                    headers=headers,
+                                    params=params)
+
+        return response
+
+    def create_request_history(self, method, url, response, payload=None,
+                               headers=None, params=None):
+        values = {
+            'config_id': self.id,
+            'method': method,
+            'url': url,
+            'payload': payload,
+            'request_headers': headers,
+            'response_headers': response.headers,
+            'params': params,
+            'response_code': response.status_code,
+        }
+        try:
+            values.update(message=json.loads(response.content))
+        except json.decoder.JSONDecodeError:
+            pass
+        self.env['ipf.final_report.request.history'].create(values)
+
+    def get_headers(self):
+        tracking_id = pycompat.text_type(uuid.uuid1())
+        ipf_system_id = (
+            self.env["ir.config_parameter"].sudo().get_param("api_ipf.ipf_system_id")
+        )
+        headers = {
+            'Content-Type': "application/json",
+            'AF-TrackingId': tracking_id,
+            'AF-SystemId': ipf_system_id,
+            'AF-EndUserId': "*sys*",
+            'AF-Environment': self.environment,
+        }
+        return headers
+
+    def get_url(self, path):
+        if self.url[-1] == '/':
+            url = self.url + path
+        else:
+            url = self.url + '/' + path
+        return url
+
+    def post_report(self, payload):
+        querystring = {"client_secret": self.client_secret,
+                       "client_id": self.client_id}
+        url = self.get_url('v1/slutredovisning')
+        response = self.request_call(method="POST",
+                                     url=url,
+                                     payload=json.dumps(payload),
+                                     headers=self.get_headers(),
+                                     params=querystring)
+        return response
+
+    def test_post_report(self):
+        payload = {
+            "utforande_verksamhets_id": "10011119",
+            "avrops_id": "A000000428847",
+            "genomforande_referens": "100003568",
+            "ordernummer": "MEET-1",
+            "personnr": "199910103028",
+            "unikt_id": "1421",
+            "inskickad_datum": "2020-12-22",
+            "rapportering_datum": "2020-12-22",
+            "status": "10",
+            "sent_inskickad":"false",
+            "deltagare": {
+                "fornamn": "testperson",
+                "efternamn": "testson",
+                "deltog_per_distans": "false"
+            },
+            "ansvarig_handledare": {
+                "fornamn": "John",
+                "efternamn": "Doe",
+                "signatur": "fritext"
+            },
+            "innehall": [
+                {
+                    "aktivitets_id": "176",
+                    "aktivitets_namn": "Val och framtidsplanering - deltagarens karriärplan",
+                    "beskrivning": "Coach's comment"
+                },
+                {
+                    "aktivitets_id": "177",
+                    "aktivitets_namn": "Individuella karriärsvägledningssamtal",
+                    "beskrivning": "Coach's comment"
+                },
+                {
+                    "aktivitets_id": "178",
+                    "aktivitets_namn": "Stöd till att bli antagen till kommunens insatser",
+                    "beskrivning": "Coach's comment"
+                },
+                {
+                    "aktivitets_id": "179",
+                    "aktivitets_namn": "Studiebesök utbildningsanordnare",
+                    "beskrivning": "Coach's comment"
+                },
+                {
+                    "aktivitets_id": "180",
+                    "aktivitets_namn": "Studiebesök arbetsplatser",
+                    "beskrivning": "Coach's comment"
+                },
+                {
+                    "aktivitets_id": "181",
+                    "aktivitets_namn": "Möte med förebilder.",
+                    "beskrivning": "Coach's comment"
+                },
+                {
+                    "aktivitets_id": "182",
+                    "aktivitets_namn": "Kunskap om arbetsmarknad, utbildningsvägar, studiefinansiering, omvärldsbev.",
+                    "beskrivning": "Coach's comment"
+                }
+                ],
+            "avbrott": "true",
+            "ofullstandig": "false",
+            "huvudmal": {
+                "yrkesomrade": "Hotell & Restaurang",
+                "yrke": "Kock",
+                "arbetsuppgifter_beskrivning": "Lagar mat",
+                "val_av_huvudmal_motivering": "Annat",
+                "fritext": "Används vid val_av_huvudmal_motivering Annat",
+                "steg": [{
+                    "typ": "Studera",
+                    "kompletterande_insats": {
+                        "typ": "Studiemotiverande insats"
+                    },
+                    "namn": "",
+                    "niva": "",
+                    "startdatum": "2020-12-22",
+                    "slutdatum": "2020-12-22"
+                }]
+            },
+            "alternativ_mal": {
+                "yrkesomrade": "Hotell & Restaurang",
+                "yrke": "Kock",
+                "arbetsuppgifter_beskrivning": "Lagar mat",
+                "val_av_alternativ_mal_motivering": "Matchar deltagarens intressen",
+                "steg": [{
+                "typ": "Studera",
+                "namn": "",
+                "niva": "",
+                "startdatum": "2020-12-22",
+                "slutdatum": "2020-12-22"
+                }]
+            },
+            "studiebesok": [{
+                "namn": "utbildningssamordnare",
+                "typ": "studieinriktning",
+                "motivering": "fritext"
+            }],
+            "hinder": {
+                "orsak_typ": "Annat",
+                "motivering": "Fritext, används vid orsak_typ Annat"
+            }
+        }
+
+        response = self.post_report(payload)
+        print(response.text)
+
+    @api.model
+    def get_api(self):
+        return self.search([], limit=1)
+
+    @api.model
+    def post_request(self, outplacement, res_joint_planning_af_recordset):
+        res_join = res_joint_planning_af_recordset
+        api = self.get_api()
+        if 'department_ref' in outplacement.department_id:
+            dep_id = outplacement.department_id.department_ref
+        else:
+            dep_id = outplacement.department_id.ka_ref
+        payload = {
+            "utforande_verksamhets_id": dep_id,
+            "avrops_id": outplacement.name,
+            "genomforande_referens": outplacement.order_id.origin,
+            "ordernummer": outplacement.order_id.name,
+            "personnr": outplacement.partner_id.company_registry,
+            "unikt_id": outplacement.uniq_ref,
+            "inskickad_datum": str(outplacement.jp_sent_date),
+            "rapportering_datum": "2020-12-22", #send_date? New field?
+            "status": "10", #10 = Approved, 20 = NotApproved, 30 = In-Progress, 40 = Rejected, 50 = Cancelled, 60 = Sent. New field?
+            "sent_inskickad":"false", #new field?
+            "deltagare": {
+                "fornamn": outplacement.partner_id.firstname,
+                "efternamn": outplacement.partner_id.lastname,
+                "deltog_per_distans": outplacement.meeting_remote
+            },
+            "ansvarig_handledare": {
+                "fornamn": outplacement.employee_id.firstname,
+                "efternamn": outplacement.employee_id.lastname,
+                "signatur": "" #check for signature, which field?
+            },
+            "innehall": [],
+            "avbrott": "true", #new field? hardcode to false?
+            "ofullstandig": "false", #new field? hardcode to false?
+            "huvudmal": {  #taken from ssyk, desired jobs?
+                "yrkesomrade": "Hotell & Restaurang", 
+                "yrke": "Kock", 
+                "arbetsuppgifter_beskrivning": "Lagar mat",
+                "val_av_huvudmal_motivering": "Annat", #new field?
+                "fritext": "Används vid val_av_huvudmal_motivering Annat", #new field?
+                "steg": [{ #new model?
+                    "typ": "Studera",
+                    "kompletterande_insats": {
+                        "typ": "Studiemotiverande insats"
+                    },
+                    "namn": "",
+                    "niva": "",
+                    "startdatum": "2020-12-22",
+                    "slutdatum": "2020-12-22"
+                }]
+            },
+            "alternativ_mal": { #same as above, separate field
+                "yrkesomrade": "Hotell & Restaurang",
+                "yrke": "Kock",
+                "arbetsuppgifter_beskrivning": "Lagar mat",
+                "val_av_alternativ_mal_motivering": "Matchar deltagarens intressen",
+                "steg": [{
+                "typ": "Studera",
+                "namn": "",
+                "niva": "",
+                "startdatum": "2020-12-22",
+                "slutdatum": "2020-12-22"
+                }]
+            },
+            "studiebesok": [],
+            "hinder": {
+                "orsak_typ": outplacement.obstacle_id.reason,
+                "motivering": outplacement.obstacle_id.motivation
+            }
+        }
+        for planned in self.env['res.joint_planning'].search([('send2server','=',True)],order=sequence):
+            task = outplacement.task_ids.filtered(lambda t: t.activity_id == planned.activity_id)
+            payload['innehall'].append({
+                'aktivitets_id': planned.activity_id,
+                'aktivitets_namn': task.activity_name if task else planned.activity_name,
+                'beskrivning': task.description if task else '',
+            })
+        for study_visit in outplacement.study_visit_ids:
+            payload['studiebesok'].append({
+                "namn": study_visit.name,
+                "typ": study_visit.visit_type,
+                "motivering": study_visit.reasoning 
+            })
+        api.post_report(payload)

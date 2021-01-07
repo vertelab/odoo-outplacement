b01b4bcfe1668354d771cb56fa8b1ccc8f943550
Sebastian Gieser <sebastian.gieser@vertel.se>
2021-01-07 16:19:52 +0100

    outplacement_final_report AFC-1337: added name to final report page outplacement_final_report_ipf_client AFC-1337: removed unused variables, added return value to post_request, fixed dependecies outplacement_final_report_send AFC-1337: added button to final report page to send reports.

--- outplacement_final_report_send/__manifest__.py
+++ outplacement_final_report_send/__manifest__.py
@@ -1,18 +1,17 @@
 # -*- coding: UTF-8 -*-
 # Version format OdooMajor.OdooMinor.Major.Minor.Patch
 {
-    'name': 'IPF Final Report Client',
+    'name': 'Outplacement Final Report send',
     'version': '12.0.0.1.0',
     'category': 'Outplacement',
-    'description': """This module adds the client config for final report.""",
+    'description': """This module adds a button to the outplacement interface that allows the sending of final reports.""",
 
     'author': "Vertel AB",
     'license': 'AGPL-3',
     'website': 'https://vertel.se',
-    'depends': ['outplacement'],
+    'depends': ['outplacement_final_report_ipf_client', 'outplacement_final_report'],
     'data': [
-        "security/ir.model.access.csv",
-        'views/client_config_views.xml',
+        'views/outplacement_view.xml',
     ],
     'installable': True,
     'images': [

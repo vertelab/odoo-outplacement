d934f6b19c67ffca882ee96fd82807d7b0eae286
Nils Nyman-Waara <nils.nyman-waara@vertel.se>
2021-01-10 15:37:10 +0100

    Bug fixes of account_invoice

--- outplacement_invoice/models/account_invoice.py
+++ outplacement_invoice/models/account_invoice.py
@@ -11,6 +11,7 @@ _logger = logging.getLogger(__name__)
 
 class AccountInvoice(models.Model):
     _inherit = "account.invoice"
+    raindance_ref = fields.Char(string='Raindance ID')
 
     @api.model
     def cron_outplacement_invoice(self):
@@ -24,18 +25,24 @@ class AccountInvoice(models.Model):
     def create_invoice(self, invoice):
         sf = invoice.get('svefaktura')
         if sf:
-            sf = xmltodict(sf)
+            sf = xmltodict.parse(sf)
+            invoice_ref = sf['Invoice']['ID']
+            if len(self.env['account.invoice'].search(
+                    [('raindance_ref', '=', invoice_ref)])):
+                _logger.debug('Invoice already exists: %s', invoice_ref)
+                return
             party = sf['Invoice']['cac:BuyerParty']['cac:Party']
             # Find or create a res_partner for buyer (AF).
             org_nr = party['cac:PartyIdentification']['cac:ID']
-            res_partner = self.env['res.parter'].search(
-                [('company_registry', '=', org_nr)])
-            if not res_partner:
+            res_partner = self.env['res.partner'].search(
+                [('company_registry', '=', org_nr)], limit=1)
+            if not len(res_partner):
+                _logger.warn('HERE')
                 name = party['cac:PartyName']['cbc:Name']
                 org_nr = party['cac:PartyIdentification']['cac:ID']
                 street = party['cac:Address']['cbc:Postbox']
-                city = party['cac:Address']['CityName']
-                zip_ = party['cbc:PostalZone']
+                city = party['cac:Address']['cbc:CityName']
+                zip_ = party['cac:Address']['cbc:PostalZone']
                 res_partner = self.env['res.partner'].create(
                     {'name': name,
                      'company_registry': org_nr,
@@ -45,13 +52,16 @@ class AccountInvoice(models.Model):
 
             # Create Invoice.
             current_invoice = self.env['account.invoice'].create(
-                {'partner_id': res_partner.id})
+                {'partner_id': res_partner.id,
+                 'raindance_ref': invoice_ref})
 
             # Create attachment with raw data.
             self.env['ir.attachment'].create(
-                {'res_model': 'account.invoice',
+                {'name': 'Svefaktura',
+                 'res_model': 'account.invoice',
                  'res_id': current_invoice.id,
-                 'datas': base64.b64encode(invoice)})
+                 'datas': base64.b64encode(bytes(invoice['svefaktura'],
+                                                 'utf-8'))})
 
             # Find Invoice lines to add to Invoice.
             invoice_lines = sf['Invoice']['cac:InvoiceLine']
@@ -66,8 +76,11 @@ class AccountInvoice(models.Model):
                 price_info = item['cac:BasePrice']['cbc:PriceAmount']
                 price = price_info['#text']
                 quantity = line['cbc:InvoicedQuantity']['#text']
+                account = self.env['account.account'].search(
+                    [('code', '=', '3000')], limit=1)
                 self.env['account.invoice.line'].create(
-                    {'invoice_id': current_invoice.id,
+                    {'account_id': account.id,
+                     'invoice_id': current_invoice.id,
                      'name': description,
                      'price_unit': price,
                      'quantity': quantity})

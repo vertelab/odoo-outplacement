9803587759b5f71c9e1ee532bd33eb1b307b68b8
Nils Nyman-Waara <72382478+nils-vertel@users.noreply.github.com>
2021-01-11 09:14:35 +0100

    Update __manifest__.py

--- outplacement_crmsync/__manifest__.py
+++ outplacement_crmsync/__manifest__.py
@@ -3,7 +3,6 @@
 ################################################################################
 #
 #    OpenERP, Open Source Management Solution
-#    Copyright (C) 2019 N-Development (<https://n-development.com>).
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as

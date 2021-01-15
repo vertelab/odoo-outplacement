596eafcf5e39f437d7736dc3f704af54d294a23c
SebastianGieser-Vertel <61781795+SebastianGieser-Vertel@users.noreply.github.com>
2021-01-15 14:56:52 +0100

    AFC1624 & 1566 outplacement_partner_education: Remove fields and other fixes
    * outplacement_partner_education AFC-1624: removed field for references file from view
    * outplacement_partner_education AFC-1624: fixed uploading of files
    
    * outplacement_partner_education AFC-1566: now reflects the changes made to partner_education_views
    * outplacement_partner_education AFC-1566: made education look better

--- outplacement_partner_education/models/outplacement.py
+++ outplacement_partner_education/models/outplacement.py
@@ -3,14 +3,7 @@ from odoo import models, fields
 class Outplacement(models.Model):
     _inherit = "outplacement"
 
-    sun_ids = fields.Many2many(comodel_name='res.sun',
-                               string='SUN Code', related="partner_id.sun_ids", readonly=False)
-    education_level = fields.Many2one(
-        comodel_name="res.partner.education_level",
-        string="Education level", related="partner_id.education_level", readonly=False)
-    foreign_education = fields.Boolean(string="Foreign education", related="partner_id.foreign_education", readonly=False)
-    foreign_education_approved = fields.Boolean(
-        string="Foreign education approved", related="partner_id.foreign_education_approved", readonly=False)
+    education_ids = fields.Many2many(string="Educations", comodel_name="res.partner.education", related="partner_id.education_ids", readonly=False)
     cv = fields.Binary('CV', related="partner_id.cv", readonly=False)
     cv_file_name = fields.Char(related="partner_id.cv_file_name", readonly=False)
     references = fields.Binary(related="partner_id.references", readonly=False)

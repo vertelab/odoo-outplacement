5e4113e9911d205f8877105cb2e24c6c327f491d
Artur Chilangwa <artur.chilangwa@vertel.se>
2021-01-28 15:19:36 +0100

    outplacement_order_interpreter: Rename from outplacement_order_interpretor

---
+++ outplacement_order_interpreter/models/outplacement.py
@@ -0,0 +1,12 @@
+from odoo import models, fields
+
+
+class Outplacement(models.Model):
+    _inherit = "outplacement"
+
+    interpreter_language = fields.Many2one(
+        comodel_name="res.interpreter.language",
+        related="partner_id.interpreter_language",
+        readonly=False)
+    interpreter_gender_preference = fields.Selection(
+        related="partner_id.interpreter_gender_preference", readonly=False)

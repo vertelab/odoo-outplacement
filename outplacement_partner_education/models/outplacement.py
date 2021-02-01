54e4a4255dd9b0d7fe6f54090af7bc0119062820
Sebastian Gieser <sebastian.gieser@vertel.se>
2021-02-01 10:21:34 +0100

    outplacement_partner_education: fixed relation to be in line with partner_education

--- outplacement_partner_education/models/outplacement.py
+++ outplacement_partner_education/models/outplacement.py
@@ -3,7 +3,7 @@ from odoo import models, fields
 class Outplacement(models.Model):
     _inherit = "outplacement"
 
-    education_ids = fields.Many2many(string="Educations", comodel_name="res.partner.education", related="partner_id.education_ids", readonly=False)
+    education_ids = fields.One2many(string="Educations", comodel_name="res.partner.education", inverse_name="partner_id", related="partner_id.education_ids", readonly=False)
     cv = fields.Binary('CV', related="partner_id.cv", readonly=False)
     cv_file_name = fields.Char(related="partner_id.cv_file_name", readonly=False)
     references = fields.Binary(related="partner_id.references", readonly=False)

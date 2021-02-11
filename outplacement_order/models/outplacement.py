ce3fa76ba83e60d0dabfa9be5e959f43e06be679
Daniel Eriksson <daniel.eriksson@arbetsformedlingen.se>
2021-02-11 13:33:18 +0100

    outplacement_order AFC-1339: rewrote code to be cleaner and to not update outplacement.stage_id

--- outplacement_order/models/outplacement.py
+++ outplacement_order/models/outplacement.py
@@ -2,7 +2,7 @@
 
 import logging
 from odoo import api, models, fields, tools, _
-from odoo.exceptions import UserError, Warning
+from odoo.exceptions import Warning
 
 _logger = logging.getLogger(__name__)
 
@@ -12,9 +12,8 @@ class Outplacement(models.Model):
 
     @api.model
     def cron_outplacement_order(self):
-        try:
-            client_config = self.env["ipf.showorder.client.config"].search([])[0]
-        except:
+        client_config = self.env["ipf.showorder.client.config"].search([], limit=1)
+        if not client_config:
             raise Warning(_("No client config for showorder integration"))
         if not client_config.url:
             raise Warning(_("No url configured for showorder client config"))
@@ -30,38 +29,27 @@ class Outplacement(models.Model):
         for outplacement in outplacements:
             # send request
             res = client_config.get_order_id(outplacement.name)
+            _logger.warn("DAER res: %s" % res)
             order_status = res.get("orderstatus")
             if not order_status:
                 _logger.warn(
                     "Error in communication with ordertjänsten: res = %s" % res
                 )
                 raise Warning(_("Error in communication with ordertjänsten"))
-            if order_status == "AVBRUTEN":
-                outplacement.stage_id = self.env.ref("outplacement.cancelled_stage").id
-                outplacement.order_id.state = "cancel"
-            elif order_status == "AVBRUTEN_KLAR":
-                outplacement.stage_id = self.env.ref("outplacement.cancelled_stage").id
-                outplacement.order_id.state = "cancel"
-            elif order_status == "PRELIMINAR":
-                outplacement.stage_id = self.env.ref(
-                    "outplacement.not_approved_stage"
-                ).id
-                outplacement.order_id.state = "draft"
-            elif order_status == "DEFINITIV":
-                outplacement.stage_id = self.env.ref("outplacement.approved_stage").id
-                outplacement.order_id.state = "sale"
-            elif order_status == "MAKULERAD":
-                outplacement.stage_id = self.env.ref("outplacement.reject_stage").id
-                outplacement.order_id.state = "cancel"
-            elif order_status == "LEVERERAD":
-                outplacement.stage_id = self.env.ref("outplacement.sent_stage").id
-                outplacement.order_id.state = "sent"
-            elif order_status == "KLAR":
-                outplacement.stage_id = self.env.ref("outplacement.approved_stage").id
-                outplacement.order_id.state = "done"
+
+            # storing this information on outplacement.stage is wierd 
+            # now that we dont update outplacement.stage_id from here.
+            outplacement_stage = self.env['outplacement.stage'].search([('ordertjansten_status', 'ilike', order_status)], limit=1)
+            if not outplacement_stage:
+                _logger.warn("Unmapped status from ordertjänsten: order status = %s" % order_status)
             else:
-                # log this so we can catch errors in the future
-                _logger.warn(
-                    "Ordertjänsten is sending us nonsense: order status = %s"
-                    % order_status
-                )
+                # TODO: Decide if we should ever update 
+                # outplacement.stage_id from this code
+                # outplacement.stage_id = outplacement_stage.id
+                if outplacement_stage.order_id_state:
+                    if outplacement.order_id:
+                        outplacement.order_id.state = outplacement_stage.order_id_state
+                    else:
+                        _logger.warn("Outplacement does not have a sale order: %s" % outplacement.name)
+                else:
+                    _logger.warn("Unmapped saleorder status: outplacement.stage = %s" % outplacement_stage)
---
+++ outplacement_order/models/outplacement_stage.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+import logging
+from odoo import api, models, fields, tools, _
+
+_logger = logging.getLogger(__name__)
+
+
+class OutplacementStage(models.Model):
+    _inherit = 'outplacement.stage'
+
+    ordertjansten_status = fields.Char(string='Ordertjänsten status', help="Technical name of mapped status in ordertjänsten, seperate several statuses with ','") #Tekniskt namn på mappad status i ordertjänsten
+    order_id_state = fields.Char(string='Säljorder status', help='Techincal name of mapped state on saleorders in Odoo') #Tekniskt namn på mappad status på säljorder i Odoo
+    

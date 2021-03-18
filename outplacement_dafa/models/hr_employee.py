9b9a6edc82569f09f235f0812ee3226c75f7faed
Robin Chatfield <robin.chatfield@vertel.se>
2021-03-18 08:56:03 +0100

    AFC-1747 outplacement_dafa: Changed operation membership logic. Added translations.

--- outplacement_dafa/models/hr_employee.py
+++ outplacement_dafa/models/hr_employee.py
@@ -21,7 +21,7 @@
 ###############################################################################
 
 from odoo import fields, models, api, _
-from odoo.exceptions import ValidationError
+from odoo.exceptions import AccessError
 
 import logging
 _logger = logging.getLogger(__name__)
@@ -47,19 +47,38 @@ class Employee(models.Model):
     # regular related field.
     @api.multi
     def _write_performing_operations(self):
-        permission_check = self.env.user._is_system() or \
-            self.env.user.has_group(
-                'base_user_groups_dafa.group_dafa_org_admin_write') or \
-            self.env.user.has_group(
-                'base_user_groups_dafa.group_dafa_employees_write')
-        if not permission_check:
-            raise ValidationError(_("You are not permitted to do this"))
+        """ Check if user is allowed to change operations, and sync them to user.
+        """
+        permission_lvl = (self.env.user._is_system() or self.env.user.has_group('base_user_groups_dafa.group_dafa_org_admin_write')) and 2
+        permission_lvl = permission_lvl or (self.env.user.has_group('base_user_groups_dafa.group_dafa_employees_write') and 1)
+
+        if not permission_lvl:
+            raise AccessError(_("You are not permitted to change operations."))
         for employee in self:
-            if employee.user_id:
-                employee.user_id.sudo().write({
-                    'performing_operation_ids': [
-                        (6, 0, employee.mapped('performing_operation_ids.id'))
-                        ]})
+            if not employee.user_id:
+                continue
+            if permission_lvl == 1:
+                # This user is only allowed to control own operations.
+                # Compare new values to current data in DB.
+                # Fetch current operations with SQL to avoid magic caching.
+                self.env.cr.execute(
+                    "SELECT operation_id FROM performing_operation_user_rel WHERE user_id = %s",
+                    (employee.user_id.id,))
+                old_ops = set(x['operation_id'] for x in self.env.cr.dictfetchall())
+                cur_ops = set(employee.mapped('performing_operation_ids.id'))
+                # Added operations
+                changed_ops = cur_ops - old_ops
+                # Removed operations
+                changed_ops |= (old_ops - cur_ops)
+                forbidden_ops = changed_ops - set(self.env.user.mapped('performing_operation_ids.id'))
+                if forbidden_ops:
+                    op_names = ', '.join([o['name'] for o in self.env['performing.operation'].search_read(
+                        [('id', 'in', list(forbidden_ops))], ['name'])])
+                    raise AccessError(_("You are not allowed to control these operations: {ops}.").format(ops=op_names))
+            employee.user_id.sudo().write({
+                'performing_operation_ids': [
+                    (6, 0, employee.mapped('performing_operation_ids.id'))
+                    ]})
 
     @api.model
     def _search_performing_operations(self, op, value):

0916ec5e63e06bd9c97d5e6bded8e022ef57d15c
Fredrik Arvas, Vertel AB <61729317+fredrikarvas-vertel@users.noreply.github.com>
2021-03-23 14:33:49 +0100

    Update __manifest__.py

--- outplacement_joint_planning/__manifest__.py
+++ outplacement_joint_planning/__manifest__.py
@@ -27,9 +27,9 @@
     'category': 'Outplacement',
     'description': """
     Module to handle the GUI for SubOrder (SV: Avrop ).
-     v12.0.1.0.3 adds limitation for sending GP before day 6 in the service. AFC-1943
-     v12.0.1.0.4 Fixed bugs in date comparison.
-     v12.0.1.0.5 AFC-2002 changed calculation base for the 6th day in service
+     v12.0.1.0.3 adds limitation for sending GP before day 6 in the service. AFC-1943\n
+     v12.0.1.0.4 Fixed bugs in date comparison.\n
+     v12.0.1.0.5 AFC-2002 changed calculation base for the 6th day in service\n
 """,
     'author': 'Vertel AB',
     'license': 'AGPL-3',

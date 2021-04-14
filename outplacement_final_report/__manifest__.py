40a6df75e7304a4dd54eeba1ddf8ebaabfb19b1b
Sebastian Gieser <sebastian.gieser@arbetsformedlingen.se>
2021-04-14 14:35:10 +0200

    outplacement_final_report, outplacement_final_report_ipf_client, outplacement_final_report_send AFC-2071: updated manifest

--- outplacement_final_report/__manifest__.py
+++ outplacement_final_report/__manifest__.py
@@ -1,11 +1,13 @@
 {
     'name': 'Outplacement final report',
-    'version': '12.0.0.1.2',
+    'version': '12.0.0.1.3',
     'category': 'Outplacement',
     'description': """
-	 Module to handle outplacement (Avrop) final reports.
-	 v12.0.0.1.1 Translation and gui fix for eductation
-     v12.0.0.1.2 changed some field names
+Module to handle outplacement (Avrop) final reports.
+===========================================================
+v12.0.0.1.1 Translation and gui fix for eductation
+v12.0.0.1.2 changed some field names
+v12.0.0.1.3 Updated views, relations and translations
     """,
     'author': 'Vertel AB',
     'license': 'AGPL-3',
--- outplacement_final_report_ipf_client/models/client_config.py
+++ outplacement_final_report_ipf_client/models/client_config.py
@@ -363,7 +363,7 @@ class ClientConfig(models.Model):
             for step_id in goal_id.step_ids:
                 step = {
                     "typ": step_id.step_type,
-                    "namn": step_id.other_step_name if step_id.other_step_name else "",
+                    "namn": step_id.step_name if step_id.step_name else "",
                     "niva": step_id.level if step_id.level else "",
                     "startdatum": str(step_id.start_date),
                     "slutdatum": str(step_id.end_date)

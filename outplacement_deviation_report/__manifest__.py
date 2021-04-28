3b4eb2f95ee28659a74e10f640299dbd3fe562e5
Sebastian Gieser <sebastian.gieser@arbetsformedlingen.se>
2021-04-28 16:14:41 +0200

    outplacement_deviation_report AFC-2157: updated manifest

--- outplacement_deviation_report/__manifest__.py
+++ outplacement_deviation_report/__manifest__.py
@@ -1,7 +1,7 @@
 # Version format OdooMajor.OdooMinor.Major.Minor.Patch
 {
     'name': 'Deviation Report',
-    'version': '12.0.0.1.3',
+    'version': '12.0.0.1.4',
     'category': 'Outplacement',
     "description": """
 Outplacement Deviation Report\n
@@ -9,6 +9,7 @@ Outplacement Deviation Report\n
 v12.0.0.1.1: Versions before version handling\n
 v12.0.0.1.2 AFC-2104: Changed "sent"-message in chatter box\n
 v12.0.0.1.3 improved error messages
+v12.0.0.1.4 added checks and error messages
 """,
     'author': "Vertel AB",
     'license': 'AGPL-3',

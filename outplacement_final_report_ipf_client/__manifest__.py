6af3b77f1b16006111ac620e18f0339f1a389db6
Rupareliya Hemangi <rupareliyahemangi145@gmail.com>
2021-05-26 16:04:22 +0530

    [ADD] outplacement_final_report_ipf_client: AFC-2368 Added 'Cancelled by AF' label in Outplacement kanban view if outplacement 'Interrupted'

--- outplacement_final_report_ipf_client/__manifest__.py
+++ outplacement_final_report_ipf_client/__manifest__.py
@@ -2,7 +2,7 @@
 # Version format OdooMajor.OdooMinor.Major.Minor.Patch
 {
     'name': 'IPF Final Report Client',
-    'version': '12.0.0.1.7',
+    'version': '12.0.0.1.8',
     'category': 'Outplacement',
     'description': """
 This module adds the client config for final report.
@@ -13,6 +13,7 @@ V12.0.0.1.4 fixed a value, refactoring
 V12.0.0.1.5 fixed values, added checks
 V12.0.0.1.6 fixed the value "Kompletterar tidigare erfarenet" in "val_av_alternativt_mal_motivering" in "alternativt_mal" payload
 v12.0.0.1.7 fixed the ability to send with no goals set when interrupted
+v12.0.0.1.8 AFC-2368 Added 'Cancelled by AF' label in Outplacement kanban view if outplacement 'Interrupted'.
     """,
 
     'author': "Vertel AB",

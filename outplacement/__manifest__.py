bf5c829080b0f76b6e6e51ab42475c2d2be42086
Rupareliya Hemangi <rupareliyahemangi145@gmail.com>
2021-05-29 12:50:39 +0530

    [ADD] outplacement, sale_outplacement: AFC-2419 Added Source document in search view and Added Name in list view of Outplacement.

--- outplacement/__manifest__.py
+++ outplacement/__manifest__.py
@@ -23,14 +23,15 @@
 
 {
     'name': 'Outplacement',
-    'version': '12.0.1.2.4',
+    'version': '12.0.1.2.5',
     'category': 'Outplacement',
     'description': """
 	 Module to handle outplacement (Avrop)\n
 	 v12.0.1.2.0 - changed version number to standard-format.\n
 	 v12.0.1.2.1 - AFC-1116 Changes due to Performing Operations.\n
 	 v12.0.1.2.3 - AFC-2120 Made some fields hidden outside of debug mode.\n
-     v12.0.1.2.4 - AFC-2028 Made some fields readonly.
+     v12.0.1.2.4 - AFC-2028 Made some fields readonly.\n
+     v12.0.1.2.5 - AFC-2319 Added Name in Outplacement Tree view.\n
     """,
     'author': 'Vertel AB',
     'license': 'AGPL-3',

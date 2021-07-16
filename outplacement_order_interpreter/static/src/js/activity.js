510ba6cba90723f42426872e547dfdf95447cbcd
Rupareliya Hemangi <rupareliyahemangi145@gmail.com>
2021-07-16 18:02:38 +0530

    [ADD] outplacement_order_interpreter: AFC-2593 Added Interpreter name & Phone and Deliver interpreter button on Cancellation popup.

--- outplacement_order_interpreter/static/src/js/activity.js
+++ outplacement_order_interpreter/static/src/js/activity.js
@@ -34,16 +34,19 @@ var Activity = MailActivity.include({
             let dialog = new Dialog(this, {'title': 'Avboka tolkbokningen',
                                            '$content': $('<p>
                                                          <b>Avboka tolkbokningen</b> <br>
-                                                         Avbokning sker i två steg: <br/>
+                                                         Avbokning sker i tre steg: <br/>
                                                          1 Informera tolken om att ni inte kan använda tiden.<br/>
+                                                         Tolkens namn : &nbsp'+activity.interpreter_name+'<br/>
+                                                         Tolkens telefonnummer: &nbsp'+activity.interpreter_phone+'<br/>
                                                          2 Skicka ett epostmeddelande till&nbsp<a href="mailto:team-crm@arbetsformedlingen.se?subject=Avboka tolk med referens ' + activity.interpreter_booking_ref + ' och KA-Nr: ' + activity.interpreter_ka_nr + '."> team-crm@arbetsformedlingen.se</a> &nbsp och ange referensnummer&nbsp'+ activity.interpreter_booking_ref +'.<br/>
                                                          Aktuellt KA-Nr:&nbsp'+activity.interpreter_ka_nr+'.<br/>
                                                          3 Klicka på inleverera tolken.<br/>
                                                          </p>'),
                                            'technical':'false',
                                            'buttons':[
-//                                               {'text':'Markera som Avbokad', 'close':'true', 'click':function(){self.interpreter_cancel(options)}},
-                                               {'text':'Stäng rutan utan att markera som avbokad', 'close':'true', 'classes':'btn-primary'}]})
+                                               {'text':'Inleverera tolken', 'close':'true', 'classes':'btn-primary',
+                                               'click':function(){self.interpreter_deliver(options, activity)}},
+                                               {'text':'Stäng', 'close':'true'}]})
             dialog.open()
             return
         };
@@ -54,10 +57,23 @@ var Activity = MailActivity.include({
             })
             .then(this._reload.bind(this, {activity: true}));
     },
-    interpreter_cancel: function (options){
+    interpreter_deliver: function (options, activity){
         // Unlink, logg, reload.
-        this._rpc({model: options.model, method: 'interpreter_cancel_booking', args: options.args})
-        .then(this._reload.bind(this, {activity: true}));
+        var action = {
+            type: 'ir.actions.act_window',
+            res_model: 'outplacement.interpreter_delivery.wizard',
+            view_mode: 'form',
+            view_type: 'form',
+            views: [[false, 'form']],
+            target: 'new',
+            context: {
+                default_res_id: activity.id,
+                default_res_model: options.model,
+                active_id: activity.id
+            },
+        };
+        var callback = this._reload.bind(this, { activity: true, thread: true });
+        return this.do_action(action, { on_close: callback });
     },
 });
 
